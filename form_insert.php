62fa50bf2edf42395fd07ac11e03b2961739aa60
KhoaLe3m <95561146+KhoaLe3m@users.noreply.github.com>
2022-03-18 10:47:41 +0700

    test

---
+++ form_insert.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<form method="post" action="form_process.php">
+		Tieu De:
+		<input type="text" name="tieu_de">
+		<br>
+		Noi Dung
+		<textarea name="noi_dung"></textarea>
+		<br>
+		Link anh
+		<input type="text" name="anh">
+		<br>
+		<button>Them</button>
+	</form>
+</body>
+</html>
